dd3929236d4e5dc6ad2c61d5900656ec1e223949
Bounor <l.bounor@hadlyon.asso.fr>
2021-11-10 11:00:49 +0100

    add ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,15 +6,45 @@
 /*   By: Leo <Leo@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 17:51:04 by Leo               #+#    #+#             */
-/*   Updated: 2021/11/08 23:41:50 by Leo              ###   ########lyon.fr   */
+/*   Updated: 2021/11/10 10:51:58 by Leo              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.c"
+#include "libft.h"
+
+static void	ft_except(int n, int fd)
+{
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
+		ft_putchar_fd('0', fd);
+}
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
+	int		i;
+	char	str[10];
 
-	
+	if (fd < 0)
+		return ;
+	if (n == 0 || n == -2147483648)
+	{
+		ft_except(n, fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	i = 0;
+	while (n > 0)
+	{
+		str[i] = n % 10 + '0';
+		n = n / 10;
+		i++;
+	}
+	i--;
+	while (i >= 0)
+		ft_putchar_fd(str[i--], fd);
 }
